db4b0edfbbde4e57742ff4d731c0c4a97e7ed4eb
Lukas Pierce <kosha.industry@gmail.com>
2017-07-20 01:53:18 +0600

    add Region model

---
+++ app/Model/Region.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Model_City город
+ *
+ * @property int           id
+ * @property string        alias
+ * @property string        title
+ *
+ * @property Model_Country country
+ * @property int           country_id
+ *
+ * @property ORM           cities
+ */
+class Region extends Model {
+
+  public function country() {
+    return $this->belongsTo('App\Model\Country');
+  }
+
+  //todo relationship
+  protected $_has_many = [
+    'cities' => [
+      'model'       => 'City',
+      'foreign_key' => 'region_id',
+    ],
+  ];
+
+  //todo form validation
+  public function rules() {
+    return [
+      'title'      => [
+        ['not_empty'],
+        [[$this, 'unique'], ['title', ':value']],
+      ],
+      'country_id' => [
+        ['not_empty'],
+      ],
+    ];
+  }
+
+  //public static function exists($id) {
+  //  return ORM::factory('Region', $id)->loaded();
+  //}
+  //
+  //public function delete() {
+  //
+  //  foreach ($this->cities->find_all() as $city) {
+  //    $city->delete();
+  //  }
+  //
+  //  return parent::delete();
+  //}
+
+//  public static function route_filter_alias($route, $params, $request) {
+//
+//    $alias = Arr::get($params, 'city_alias');
+//    $city = ORM::factory('City', ['alias' => $alias]);
+//    $request->city = $city;
+//
+//    if (!empty($alias)) return $city->loaded();
+//
+//    return null;
+//  }
+
+
+}
