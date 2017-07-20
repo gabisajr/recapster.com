f63ece414914a96cf49e59f8f1d9b4543bb1c368
Lukas Pierce <kosha.industry@gmail.com>
2017-07-20 12:17:17 +0600

    add migration: create currencies table

---
+++ database/migrations/2017_07_20_060924_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('currencies', function (Blueprint $table) {
+
+      $table->string('code', 3)->unique();
+      $table->primary('code');
+
+      $table->string('title')->unique();
+      $table->string('symbol', 1)->nullable()->comment("символ волюты");
+      $table->string("short", 5)->nullable()->comment("сокращеное название (тг, руб)");
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('currencies');
+  }
+}
