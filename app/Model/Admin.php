19955368a2c57497afc5221d15d7f228c0747b99
Lukas Pierce <kosha.industry@gmail.com>
2017-07-21 05:14:04 +0600

    add Admin model

---
+++ app/Model/Admin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\Admin\ResetPassword as ResetPasswordNotification;
+
+class Admin extends Authenticatable {
+
+  use Notifiable;
+
+  protected $guard = 'admin';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password', 'job_title',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  /**
+   * Send the password reset notification.
+   *
+   * @param  string $token
+   * @return void
+   */
+  public function sendPasswordResetNotification($token) {
+    $this->notify(new ResetPasswordNotification($token));
+  }
+
+}
