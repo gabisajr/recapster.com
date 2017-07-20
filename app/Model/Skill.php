64dd5f5d23dc4530878b9eb2c7502df2b0e5969c
Lukas Pierce <kosha.industry@gmail.com>
2017-07-20 12:35:56 +0600

    add Skill model

---
+++ app/Model/Skill.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Model_Skill - Навык
+ * @property int    $id
+ * @property string $title
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Skill extends Model {
+
+  public function rules() { //todo validation
+    return [
+      'title' => [
+        ['not_empty'],
+        [[$this, 'unique'], ['title', ':value']],
+      ],
+    ];
+  }
+
+}
