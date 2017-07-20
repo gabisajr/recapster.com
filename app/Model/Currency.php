0b8315b3327ebdc4395d5445186569a71f6d2acc
Lukas Pierce <kosha.industry@gmail.com>
2017-07-20 12:19:44 +0600

    add Currency model

---
+++ app/Model/Currency.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Currency
+ *
+ * @property string $code
+ * @property string $title
+ * @property string $symbol - символ волюты
+ * @property string $short  - сокращеное название (тг, руб)
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Currency extends Model {
+
+  protected $primaryKey = "code";
+
+
+  //public static function exists($code) {
+  //  return ORM::factory('Currency', $code)->loaded();
+  //}
+
+  /**
+   * получить полулярные валюты
+   *
+   * @return Model_Currency[]
+   */
+  public function get_top_currencies() { //todo camelCase
+    $top_currencies = ['KZT', 'EUR', 'USD', 'RUB'];
+
+    return ORM::factory('Currency')
+      ->where('code', 'IN', $top_currencies)
+      ->select([DB::expr(
+        "(IF(`code` = 'KZT', 4,
+          IF(`code` = 'USD', 3,
+          IF(`code` = 'EUR', 2,
+          IF(`code` = 'RUB', 1, 0)))))"), 'priority_weight'])
+      ->order_by('priority_weight', 'DESC')
+      ->order_by('title')
+      ->find_all();
+  }
+
+}
