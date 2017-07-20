49012cc08565b520c9da39d69035fe5abb1826b0
Lukas Pierce <kosha.industry@gmail.com>
2017-07-21 04:56:13 +0600

    add admin guard

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,11 @@ return [
       'driver'   => 'token',
       'provider' => 'users',
     ],
+
+    'admin' => [
+      'driver'   => 'session',
+      'provider' => 'admins',
+    ],
   ],
 
   /*
@@ -68,6 +73,11 @@ return [
     'users' => [
       'driver' => 'eloquent',
       'model'  => App\Model\User::class,
+    ],
+
+    'admins' => [
+      'driver' => 'eloquent',
+      'model'  => App\Model\Admin::class,
     ],
 
     // 'users' => [
@@ -96,6 +106,11 @@ return [
       'provider' => 'users',
       'table'    => 'password_resets',
       'expire'   => 60,
+    ],
+    'admins' => [
+      'provider' => 'admins',
+      'table'    => 'password_resets',
+      'expire'   => 15,
     ],
   ],
 
