e0bcea00b5e5247b6d0af717a3e56378169bcd88
Lukas Pierce <kosha.industry@gmail.com>
2017-07-21 00:15:57 +0600

    add vk config

---
+++ config/vk.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'client_id'     => "5243744", # идентификатор Вашего приложения
+  'client_secret' => "IhBq18kSJeytPFiGPsXd", # секретный ключ
+  'redirect_uri'  => "http://www.recapster.com/vkauth/verify", # путь на который ВКонтакте перенаправит пользователя
+  'version'       => '5.44', # версия API ВКонтакте
+  'group_url'     => 'https://vk.com/recapster', # группа ВКонтакте
+  'group_id'      => '112941564', # id группы
+];
