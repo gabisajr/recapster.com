c26075646b3ec463ffeba25f937c7e63c97d83a9
Lukas Pierce <kosha.industry@gmail.com>
2017-07-23 20:48:47 +0600

    register LoadCompany middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,11 +49,12 @@ class Kernel extends HttpKernel {
    * @var array
    */
   protected $routeMiddleware = [
-    'auth'       => \Illuminate\Auth\Middleware\Authenticate::class,
-    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-    'bindings'   => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    'can'        => \Illuminate\Auth\Middleware\Authorize::class,
-    'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,
-    'throttle'   => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    'auth'         => \Illuminate\Auth\Middleware\Authenticate::class,
+    'auth.basic'   => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+    'bindings'     => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    'can'          => \Illuminate\Auth\Middleware\Authorize::class,
+    'guest'        => \App\Http\Middleware\RedirectIfAuthenticated::class,
+    'throttle'     => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    'load.company' => \App\Http\Middleware\LoadCompany::class,
   ];
 }
