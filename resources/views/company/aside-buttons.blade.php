88b8c75faf98f346509d79b33f28fbdaec9e74fe
Lukas Pierce <kosha.industry@gmail.com>
2017-07-27 02:04:58 +0600

    add company aside buttons view

---
+++ resources/views/company/aside-buttons.blade.php
@@ -0,0 +1,17 @@
+<div class="buttons_wrapper clear">
+
+  {{--subscribe button --}}
+  @php
+    /** @var \App\Model\User $currUser */
+    $currUser = Auth::getUser();
+    $subscribed = $currUser && $currUser->subscribedOnCompany($company); //is_company_subscribed
+
+    $cssClass = 'btn btn-block btn-subscribe btn-subscribe-company';
+    if ($subscribed) $cssClass .= ' btn-success';
+
+    $attributes = html_attributes(['class' => $cssClass, 'data-id' => $company->id]);
+    $btnText = $subscribed ? __('Вы подписаны') : __('Подписаться');
+  @endphp
+  <button{!! $attributes !!}>{{ $btnText }}</button>
+
+</div>
