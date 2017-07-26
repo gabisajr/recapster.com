c70c3eea6d02b4bfd8092f207f75dc0c89ae65f5
Lukas Pierce <kosha.industry@gmail.com>
2017-07-27 02:00:46 +0600

    add company aside view

---
+++ resources/views/company/aside.blade.php
@@ -0,0 +1,49 @@
+@php
+  /**
+   * @var \App\Model\Company $company
+   * @var Model_User         $curr_user
+   * @var View               $info
+   * @var array              $buttons
+   */
+
+  $rating = view('partials.mark-widget', ['rating' => $company->rating, 'show_number' => true])->render();
+  $btnHtml = view('company.aside-buttons', ['company' => $company])->render();
+
+@endphp
+<div class="panel">
+  <div class="panel-body">
+    <aside class="profile-aside company-aside">
+
+      <div class="badge">
+        @php $bg = $company->cover ? $company->cover->path : null @endphp
+        <div class="badge-bg{{ $bg ? ' has-bg' : '' }}" style="{{ $bg ? "background-image: url($bg)" : '' }}"></div>
+        <div class="logo-wrapper">
+          <img src="{{ logo_big($company, 640) }}" class="logo" alt="{{ $company->title }}">
+        </div>
+        <div class="badge-right visible-xs">
+          <?=$rating?>
+        </div>
+      </div>
+
+      <div class="hidden-xs">{!! $btnHtml !!}</div>
+
+      <h1 class="clear profile-aside-title">
+        <a class="hover_opacity" href="{{ $company->url() }}">{!! insert_br($company->title) !!}</a>
+        {!! icon_confirmed_company($company) !!}
+      </h1>
+
+      <div>
+        @if($company->short_desc)
+          {{ $company->short_desc }}
+        @else
+          {{ $company->created_at->diffForHumans() . " на " . config('app.name') }}
+        @endif
+      </div>
+
+      <div class="hidden-xs marg-top-sm marg-bot">{!! $rating !!}</div>
+
+      <div class="visible-xs marg-top">{!! $btnHtml !!}</div>
+
+    </aside>
+  </div>
+</div>
