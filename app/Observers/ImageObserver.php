32dc848e16acc32a67b1015cebe9e0af30b7da8e
Lukas Pierce <kosha.industry@gmail.com>
2017-07-28 02:59:12 +0600

    add ImageObserver.php for cascade deleting image childs

---
+++ app/Observers/ImageObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Lukas
+ * Date: 16.03.2017
+ * Time: 18:21
+ */
+
+namespace App\Observers;
+
+use App\Model\Image;
+use Illuminate\Support\Facades\File;
+
+class ImageObserver {
+
+  public function deleting(Image $image) {
+
+    foreach ($image->childs()->get() as $child) {
+      $child->delete();
+    }
+
+    File::delete(public_path($image->path));
+  }
+
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,26 @@
 
 namespace App\Providers;
 
+use App\Model\Image;
+use App\Observers\ImageObserver;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot() {
+    Image::observe(ImageObserver::class);
+  }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register() {
+    //
+  }
 }
