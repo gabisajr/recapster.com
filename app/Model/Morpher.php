f1766ce2ef9482b6ba4b5dd8d2e11036206d84a0
Lukas Pierce <kosha.industry@gmail.com>
2017-07-27 23:52:37 +0600

    add static inflect method to Morpher model

--- app/Model/Morpher.php
+++ app/Model/Morpher.php
@@ -32,4 +32,50 @@ class Morpher extends Model {
   protected $table = "morpher";
   protected $primaryKey = "И";
 
+  public static function inflect($text, $padeg) {
+    if (!$text || !$padeg) return null;
+
+    $text = trim($text);
+    $padeg = mb_strtoupper($padeg);
+
+    /** @var Model_Morpher $morpher */
+    $morpher = Morpher::find($text);
+
+    if (!$morpher) {
+
+      $morpher = new Morpher();
+      $morpher->И = $text;
+
+      try {
+        $url = "http://api.morpher.ru/WebService.asmx/GetXml?s=" . urlencode($text);
+        $xmlinfo = simplexml_load_file($url);
+
+        //единственное число
+        $morpher->Р = (string)$xmlinfo->Р;
+        $morpher->Д = (string)$xmlinfo->Д;
+        $morpher->В = (string)$xmlinfo->В;
+        $morpher->Т = (string)$xmlinfo->Т;
+        $morpher->П = (string)$xmlinfo->П;
+
+        //множественное число
+        $morpher->МИ = (string)$xmlinfo->множественное->И;
+        $morpher->МР = (string)$xmlinfo->множественное->Р;
+        $morpher->МД = (string)$xmlinfo->множественное->Д;
+        $morpher->МВ = (string)$xmlinfo->множественное->В;
+        $morpher->МТ = (string)$xmlinfo->множественное->Т;
+        $morpher->МП = (string)$xmlinfo->множественное->П;
+
+      } catch (Exception $e) {
+
+      }
+
+      $morpher->save();
+
+    }
+
+    if (empty($morpher->{$padeg})) return $text;
+
+    return $morpher->{$padeg};
+  }
+
 }
