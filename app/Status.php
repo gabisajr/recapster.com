96ada29b36222ffaa69ee234089637a4fbd017c8
Lukas Pierce <kosha.industry@gmail.com>
2017-07-28 10:52:38 +0600

    add static array of all statuses

--- app/Status.php
+++ app/Status.php
@@ -7,4 +7,12 @@ class Status {
   const PENDING = 'pending';
   const REJECTED = 'rejected';
   const DRAFT = 'draft';
+
+  public static $all = [
+    self::PENDING,
+    self::APPROVED,
+    self::REJECTED,
+    self::DRAFT,
+  ];
+
 }
