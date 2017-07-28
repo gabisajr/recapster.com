de6511ae57851a5a328982ea99ef48b5d58f55bd
Lukas Pierce <kosha.industry@gmail.com>
2017-07-28 13:50:39 +0600

    add admin job store route

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
   //job
   Route::get('jobs', ['as' => 'admin.jobs', 'uses' => 'JobController@list']);
   Route::get('job/create', ['as' => 'admin.job.create', 'uses' => 'JobController@create']);
+  Route::post('job/store', ['as' => 'admin.job.store', 'uses' => 'JobController@store']);
 
   //image
   Route::post('image/delete', ['as' => 'admin.image.delete', 'uses' => 'ImageController@delete']);
