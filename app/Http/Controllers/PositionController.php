79be1cc0a6e11a43f70f746694b16fdcde639d5d
Lukas Pierce <kosha.industry@gmail.com>
2017-07-30 04:03:30 +0600

    add position controller

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Position;
+use Illuminate\Http\Request;
+
+class PositionController extends Controller {
+
+  //search position
+  public function search(Request $request) {
+    $positions = Position::search($request->input('q'))->get();
+
+    $items = [];
+    /** @var Position $position */
+    foreach ($positions as $position) {
+      $items[] = [
+        'id'   => $position->id,
+        'text' => $position->title,
+      ];
+    }
+
+    $data = [
+      'items' => $items,
+    ];
+
+    return response()->json($data);
+  }
+
+}
