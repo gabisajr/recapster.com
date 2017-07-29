8ba3631ac98b38f5b3331c020edd7b497b14dd25
Lukas Pierce <kosha.industry@gmail.com>
2017-07-29 22:15:51 +0600

    set sidebar active for admin vocabularies

--- app/Http/Controllers/Admin/VocabulariesController.php
+++ app/Http/Controllers/Admin/VocabulariesController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
+use View;
+
 class VocabulariesController extends AdminController {
 
+  public function __construct() {
+    parent::__construct();
+    View::share('sidebarActive', 'vocabulary');
+  }
+
   public function list() {
-    return view("admin/vocabulary/list", [
+    return view("admin.vocabulary.list", [
       'title' => __('Словари'),
     ]);
   }
