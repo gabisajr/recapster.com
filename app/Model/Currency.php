3aaad3d28113ca9a37dd307d103c0a0a1f8e4649
Lukas Pierce <kosha.industry@gmail.com>
2017-07-30 03:49:26 +0600

    add id field

--- app/Model/Currency.php
+++ app/Model/Currency.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Class Currency
  *
+ * @property int    $id
  * @property string $code
  * @property string $title
  * @property string $symbol - символ волюты
