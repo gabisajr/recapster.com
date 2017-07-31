edda9d1d9d43d5c6b67cdf73fb9afcedd22109a2
Lukas Pierce <kosha.industry@gmail.com>
2017-08-01 00:22:14 +0600

    add HTMLMnemonics class

---
+++ app/HTMLMnemonics.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class HTMLMnemonics {
+
+  const THIN_NON_BREAKING_SPACE = '&#8239;'; //узкий неразрывный пробел
+  const NON_BREAKING_SPACE = '&nbsp;'; //неразрывный пробел
+  const HAIR_SPACE = '&#8202;'; //волосяная шпация
+
+}
