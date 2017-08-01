9035f96e176c422886ea3137c26f523154ed1d04
Lukas Pierce <kosha.industry@gmail.com>
2017-08-01 23:14:10 +0600

    add admin companies list js

---
+++ resources/assets-admin/js/company/list.js
@@ -0,0 +1,15 @@
+define(['jquery', 'i18n', 'entity-list', 'highlight'], function ($, __) {
+
+  var $list = $('#companies-list');
+
+  $list.entityList({
+    removeUrl: '/admin/company/delete',
+    sortable: false,
+    removeQuestion: __('Вы действительно хотите удалить компанию :title с сайта', {
+      ':title': '<strong class="text-nowrap">"{title}"</strong>'
+    })
+  });
+
+  var search = $('#search-form').find('input[name="search"]').val();
+  if (search) $list.find('.title-cell').highlight(search);
+});
