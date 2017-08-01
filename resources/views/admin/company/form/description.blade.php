2ba6124955bc6f49f53c3afb3c7bffb3e1dc401d
Lukas Pierce <kosha.industry@gmail.com>
2017-08-01 14:49:43 +0600

    add textarea autosize for company description

--- resources/views/admin/company/form/description.blade.php
+++ resources/views/admin/company/form/description.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
   <label class="form-control-label" for="description">{{ __('Описание компании') }}</label>
   <div class="form-control-feedback">{{ $errors->first('description') }}</div>
-  <textarea name="description" id="description" class="form-control" rows="5">{{ old('description', $company->description) }}</textarea>
+  <textarea name="description" id="description" class="form-control autosize" rows="5">{{ old('description', $company->description) }}</textarea>
 </div>
