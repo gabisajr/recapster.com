9cdac42b466ae6638f8e2ae7fedd8733ca10baf3
Lukas Pierce <kosha.industry@gmail.com>
2017-08-12 01:01:18 +0600

    add verify_url

--- config/google-recaptcha.php
+++ config/google-recaptcha.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-  'sitekey' => '6Lc9XBgTAAAAALLITnxcM6-Md_R75COrpHkx06AV',
-  'secret'  => '6Lc9XBgTAAAAAJJ7JJE_9Mo25OjWxvCra7AMWbs3',
+  'sitekey'    => '6Lc9XBgTAAAAALLITnxcM6-Md_R75COrpHkx06AV',
+  'secret'     => '6Lc9XBgTAAAAAJJ7JJE_9Mo25OjWxvCra7AMWbs3',
+  'verify_url' => 'https://www.google.com/recaptcha/api/siteverify',
 ];
