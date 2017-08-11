bb8a452a27b7071323f58e24f82f35ecec3146b0
Lukas Pierce <kosha.industry@gmail.com>
2017-08-12 01:04:15 +0600

    add get_client_ip helper function

--- app/helpers.php
+++ app/helpers.php
@@ -183,3 +183,22 @@ function salary_range($object) {
 
   return $res;
 }
+
+function get_client_ip() {
+  $ipaddress = '';
+  if (getenv('HTTP_CLIENT_IP'))
+    $ipaddress = getenv('HTTP_CLIENT_IP');
+  elseif (getenv('HTTP_X_FORWARDED_FOR'))
+    $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+  elseif (getenv('HTTP_X_FORWARDED'))
+    $ipaddress = getenv('HTTP_X_FORWARDED');
+  elseif (getenv('HTTP_FORWARDED_FOR'))
+    $ipaddress = getenv('HTTP_FORWARDED_FOR');
+  elseif (getenv('HTTP_FORWARDED'))
+    $ipaddress = getenv('HTTP_FORWARDED');
+  elseif (getenv('REMOTE_ADDR'))
+    $ipaddress = getenv('REMOTE_ADDR');
+  else
+    $ipaddress = 'UNKNOWN';
+  return $ipaddress;
+}
