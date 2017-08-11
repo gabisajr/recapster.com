9bbbe5ff7440949215f0f4871c8744548fc707cc
Lukas Pierce <kosha.industry@gmail.com>
2017-08-11 23:19:25 +0600

    add email notifications view

---
+++ resources/views/notifications/email.blade.php
@@ -0,0 +1,58 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level == 'error')
+# Упс!
+@else
+# Привет!
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@if (isset($actionText))
+<?php
+    switch ($level) {
+        case 'success':
+            $color = 'green';
+            break;
+        case 'error':
+            $color = 'red';
+            break;
+        default:
+            $color = 'blue';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endif
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+<!-- Salutation -->
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+С уважением,<br>{{ config('app.name') }}
+@endif
+
+<!-- Subcopy -->
+@if (isset($actionText))
+@component('mail::subcopy')
+Если у вас возникли проблемы с кнопкой «{{ $actionText }}», скопируйте и вставьте этот URL-адрес в свой веб-браузер:
+[{{ $actionUrl }}]({{ $actionUrl }})
+@endcomponent
+@endif
+@endcomponent
