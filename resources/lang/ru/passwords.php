37c6b9020fd6c9067bc30a7a74e8fd9de6f1c947
Lukas Pierce <kosha.industry@gmail.com>
2017-08-11 23:51:16 +0600

    translate sent message for ru locale

--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -15,7 +15,7 @@ return [
 
   'password' => 'Passwords must be at least six characters and match the confirmation.',
   'reset'    => 'Your password has been reset!',
-  'sent'     => 'We have e-mailed your password reset link!',
+  'sent'     => 'Мы отправили ссылку для восстановления пароля вам на почту!',
   'token'    => 'This password reset token is invalid.',
   'user'     => "Мы не можем найти пользователя с данным Email адресом",
 
