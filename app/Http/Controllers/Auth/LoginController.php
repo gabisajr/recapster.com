51071b135a33dc2fbf5b99f44665f7791113a11c
Lukas Pierce <kosha.industry@gmail.com>
2017-08-11 22:29:35 +0600

    override showLoginForm() method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,4 +34,9 @@ class LoginController extends Controller {
   public function __construct() {
     $this->middleware('guest')->except('logout');
   }
+
+  public function showLoginForm() {
+    return view('auth.signin');
+  }
+
 }
