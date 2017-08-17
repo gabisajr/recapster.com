dc4aaffef5f2d656bcc35f9b7cb4f8759a46d163
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 02:08:38 +0600

    reformat

--- app/Model/Subscription.php
+++ app/Model/Subscription.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Model\Subscription - Подписка пользователя
  *
- * @property int                                                $id
- * @property int                                                $user_id     id пользователя
- * @property int                                                $object_id   id объекта подписки
- * @property int                                                $object_type тип объекта подписки
- * @property \Carbon\Carbon|null                                $created_at
- * @property \Carbon\Carbon|null                                $updated_at
+ * @property int $id
+ * @property int $user_id     id пользователя
+ * @property int $object_id   id объекта подписки
+ * @property int $object_type тип объекта подписки
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $object
- * @property-read \App\Model\User                               $user
+ * @property-read \App\Model\User $user
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Subscription whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Subscription whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Subscription whereObjectId($value)
