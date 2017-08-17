8ef7036861a530791e14e286b0c3fd73bf3741f0
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 02:12:40 +0600

    add subscriptions.blade.php

---
+++ resources/views/user/gag/subscriptions.blade.php
@@ -0,0 +1,8 @@
+<div class="gag">
+  <img src="{{ asset('/images/buildings.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Нет подписок') }}</strong><br>
+    {{ __('Подписывайтесь на интересные компании и отслеживайте новые вакансии, отзывы и фотографии.') }}
+  </div>
+  <a href="{{ route('companies') }}" class="btn btn-primary">{{ __('Поиск компаний') }}</a>
+</div>
