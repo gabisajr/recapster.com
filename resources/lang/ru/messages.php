7c52a35b6b16c1d1839d5f8d6c277fa75cd85a24
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 02:10:28 +0600

    add localization messages

--- resources/lang/ru/messages.php
+++ resources/lang/ru/messages.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-  'signup_user_agreement' => 'Регистрируясь, вы принимаете условия <a href=":link">Пользовательского соглашения</a>'
+  'signup_user_agreement'    => 'Регистрируясь, вы принимаете условия <a href=":link">Пользовательского соглашения</a>',
+  'suggest_complete_profile' => 'Предложите :firstname заполнить профиль, чтобы более подробно узнать о :him',
+  'show_all'                 => 'Показать все :count',
 ];
