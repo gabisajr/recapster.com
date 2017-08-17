82392f87c325f2733f5ad928d984af961536e7a0
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:47:11 +0600

    add skills.blade.php

---
+++ resources/views/user/gag/skills.blade.php
@@ -0,0 +1,7 @@
+<div class="gag">
+  <img src="{{ asset('/images/tab.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Нет информации о навыках') }}</strong><br>
+    {{ __('Укажите профессиональные навыки, которыми вы владеете и которые могут пригодиться на новой работе.') }}</div>
+  <a href="/edit/skills" class="btn btn-primary">{{ __('Добавьте ваши навыки') }}</a>
+</div>
