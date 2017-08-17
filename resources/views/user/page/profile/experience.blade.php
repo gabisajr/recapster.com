91e351e5b099cb546ef4c49c323068aaa606a0b1
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:38:51 +0600

    add experience.blade.php

---
+++ resources/views/user/page/profile/experience.blade.php
@@ -0,0 +1,25 @@
+@php
+  /**
+   * @var \App\Model\User $user
+   * @var \App\Model\UserExperience[] $experiences
+   */
+  $experiences_count = count($experiences);
+@endphp
+
+@if ($experiences_count)
+  <section class="user-profile-block" id="experience">
+    @if ($user->isMe())
+      {{--todo route--}}
+      <a href="/edit/experience" class="pencil hidden-print" title="{{ __('Редактировать') }}"></a>
+    @endif
+    <h3 class="user-profile-block-title">{{ __('Опыт работы') }}</h3>
+
+    @foreach ($experiences as $experience)
+      {{--todo use blade--}}
+      echo new Post_Experience($experience);
+    @endforeach
+
+  </section>
+@elseif ($user->isMe())
+  @include('user.gag.experience')
+@endif
