22c93c4ea639399e54eedbd434709ecdcb78c6a4
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:32:31 +0600

    add about-user.blade.php

---
+++ resources/views/user/gag/about-user.blade.php
@@ -0,0 +1,9 @@
+<div class="gag">
+  <img src="{{ asset('/images/notebook.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Нет информации о себе') }}</strong><br>
+    {{ __('Расскажите о себе так, чтобы заинтересовать интересных вам работодателей.') }}
+  </div>
+  {{--todo route--}}
+  <a href="/edit/personal" class="btn btn-primary">{{ __('Рассказать о себе') }}</a>
+</div>
