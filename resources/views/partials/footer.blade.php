8c0f99e1c383e0bf77311f408ff3affc0201de10
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:09:12 +0600

    update footer view, use flexbox engine from bootstrap 4

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,26 +19,26 @@
             <div class="links">
               <div class="links-title">{{ config('app.name') }}</div>
               <ul class="list-unstyled">
-                <li><a href="/about">О проекте</a></li>
-                <li><a href="https://vk.com/im?media=&sel=-112941564" target="_blank">Связь с нами</a></li>
-                <li><a href="/blog">Блог</a></li>
+                <li><a href="/about">{{ __("О проекте") }}</a></li>
+                <li><a href="https://vk.com/im?media=&sel=-112941564" target="_blank">{{ __("Связь с нами") }}</a></li>
+                <li><a href="/blog">{{ __("Блог") }}</a></li>
               </ul>
             </div>
 
             <div class="links">
-              <div class="links-title">Работодатель</div>
+              <div class="links-title">{{ __("Работодатель") }}</div>
               <ul class="list-unstyled">
-                <li><a href="/employers">Личный кабинет</a></li>
-                <li><a href="/add/job">Добавить работу</a></li>
+                <li><a href="/employers">{{ __("Личный кабинет") }}</a></li>
+                <li><a href="/add/job">{{ __("Добавить работу") }}</a></li>
               </ul>
             </div>
 
             <div class="links">
-              <div class="links-title">Сообщество</div>
+              <div class="links-title">{{ __("Сообщество") }}</div>
               <ul class="list-unstyled">
-                <li><a href="/guidelines">Правила сообщества</a></li>
-                <li><a href="/terms/terms-of-use">Правила использования</a></li>
-                <li><a href="/terms/privacy-policy">Политика безопасности</a></li>
+                <li><a href="/guidelines">{{ __("Правила сообщества") }}</a></li>
+                <li><a href="/terms/terms-of-use">{{ __("Правила использования") }}</a></li>
+                <li><a href="/terms/privacy-policy">{{ __("Политика безопасности") }}</a></li>
               </ul>
             </div>
           </div>
@@ -50,19 +50,28 @@
   <div class="page-footer-bot">
     <div class="container no-pad-xs">
       <div class="page-footer-content">
-        <div class="social hover_links">
-          {{--todo socials config--}}
-          <a href="<?/*=Kohana::$config->load('vk')->get('group_url')*/?>" target="_blank" class="social-vk" title="ВКонтакте"><i class="fa fa-vk"></i></a>
-          <a href="<?/*=Kohana::$config->load('facebook')->get('group_url')*/?>" target="_blank" class="social-facebook" title="Facebook"><i class="fa fa-facebook-f"></i></a>
-          <a href="<?/*=Kohana::$config->load('instagram')->get('url')*/?>" target="_blank" class="social-instagram" title="Instagram"><i class="fa fa-instagram"></i></a>
-          <a href="<?/*=Kohana::$config->load('twitter')->get('url')*/?>" target="_blank" class="social-twitter" title="Twitter"><i class="fa fa-twitter"></i></a>
-        </div>
-
-        <span class="copyright pull-left">© <?=date('Y')?>, {{ config('app.name') }}</span>
-        <div class="menu" role="menu">
-          <a href="/about">{{ __('О нас') }}</a>
-          <a href="/blog">{{ __('Блог') }}</a>
-          <a href="/rules">{{ __('Правила') }}</a>
+        <div class="row">
+          <div class="col">
+            <div class="row">
+              <div class="col col-auto"><span class="copyright">© <?=date('Y')?>, {{ config('app.name') }}</span></div>
+              <div class="col">
+                <div class="menu" role="menu">
+                  <a href="/about">{{ __('О нас') }}</a>
+                  <a href="/blog">{{ __('Блог') }}</a>
+                  <a href="/rules">{{ __('Правила') }}</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col col-auto">
+            <div class="social hover_links">
+              {{--todo socials config--}}
+              <a href="<?/*=Kohana::$config->load('vk')->get('group_url')*/?>" target="_blank" class="social-vk" title="ВКонтакте"><i class="fa fa-vk"></i></a>
+              <a href="<?/*=Kohana::$config->load('facebook')->get('group_url')*/?>" target="_blank" class="social-facebook" title="Facebook"><i class="fa fa-facebook-f"></i></a>
+              <a href="<?/*=Kohana::$config->load('instagram')->get('url')*/?>" target="_blank" class="social-instagram" title="Instagram"><i class="fa fa-instagram"></i></a>
+              <a href="<?/*=Kohana::$config->load('twitter')->get('url')*/?>" target="_blank" class="social-twitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
