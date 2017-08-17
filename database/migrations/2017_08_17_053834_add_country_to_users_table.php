f84ce1d058f4abdde615d8e3f7f48b4ffeeee588
Lukas Pierce <kosha.industry@gmail.com>
2017-08-17 11:41:48 +0600

    add migration: add 'country_id' column to 'users' table

---
+++ database/migrations/2017_08_17_053834_add_country_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCountryToUsersTable extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::table('users', function (Blueprint $table) {
+      //reference to countries table
+      $table->unsignedInteger('country_id')->nullable()->comment('id страны')->after('position_title');
+      $table->foreign('country_id')->references('id')->on('countries')->onDelete('set null')->onUpdate('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropForeign(['country_id']);
+      $table->dropColumn(['country_id']);
+    });
+  }
+}
