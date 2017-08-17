9aabbb159cb00e25ab3103845b6cfb39a2a4faa8
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:53:52 +0600

    add exams.blade.php

---
+++ resources/views/user/gag/exams.blade.php
@@ -0,0 +1,7 @@
+<div class="gag">
+  <img src="{{ asset('/images/certificate.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Нет информации') }}</strong><br>
+    {{ __('Укажите курсы, экзамены и тесты, которые вы прохдили, а также сертфикаты, которыми вы обладаете') }}</div>
+  <a href="/edit/exams" class="btn btn-primary">{{ __('Добавить информацию') }}</a>
+</div>
