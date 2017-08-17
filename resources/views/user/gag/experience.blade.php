0e170a2a3c3a8333db0292ff4231c7f86d4d145d
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:38:37 +0600

    add experience.blade.php

---
+++ resources/views/user/gag/experience.blade.php
@@ -0,0 +1,8 @@
+<div class="gag">
+  <img src="{{ asset('/images/clock.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Не указан опыт работы') }}</strong><br>
+    {{ __('Укажите компании, в которых вы работали, а также ваши должностные обязанности и достижения.') }}</div>
+  {{--todo route--}}
+  <a href="/edit/experience" class="btn btn-primary">{{ __('Добавить место работы') }}</a>
+</div>
