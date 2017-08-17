f0df47e3ea78a5e7540b676f1d9d1ccf8fffa7c8
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:30:09 +0600

    add about.blade.php

---
+++ resources/views/user/page/profile/about.blade.php
@@ -0,0 +1,17 @@
+@php
+  /**
+   * @var \App\Model\User $user
+   */
+@endphp
+
+@if ($user->about)
+  <section class="user-profile-block">
+    @if ($user->isMe())
+      <a href="/edit/personal" class="pencil hidden-print" title="{{ __('Редактировать') }}"></a>
+    @endif
+    <h3 class="user-profile-block-title hidden-xs">{{ __('О себе') }}</h3>
+    <div>{{ $user->about }}</div> {{--todo auto paragraphs, share on github--}}
+  </section>
+@elseif ($is_me)
+  @include('user/gag/about-user')
+@endif
