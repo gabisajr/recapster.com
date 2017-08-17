31fd93e9bb44c86edc336ec5dd03704024c63e3a
Lukas Pierce <kosha.industry@gmail.com>
2017-08-17 23:45:09 +0600

    add reviews_count counter helper

--- app/counts.php
+++ app/counts.php
@@ -50,6 +50,13 @@ function jobs_count($count) {
   return __("нет вакансий");
 }
 
+function reviews_count($count) {
+  if ($count) {
+    return $count . ' ' . getNumEnding($count, [__('отзыв'), __('отзыва'), __('отзывов')]);
+  }
+  return __("нет отзывов");
+}
+
 function positions_count($count) {
   if ($count) {
     return $count . ' ' . getNumEnding($count, [__('профессия'), __('профессии'), __('профессий')]);
