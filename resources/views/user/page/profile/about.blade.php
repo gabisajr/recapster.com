84892b754d6ae45f9c13ba1c65f3c67193bd4e87
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:31:18 +0600

    use user isMe method

--- resources/views/user/page/profile/about.blade.php
+++ resources/views/user/page/profile/about.blade.php
@@ -12,6 +12,6 @@
     <h3 class="user-profile-block-title hidden-xs">{{ __('О себе') }}</h3>
     <div>{{ $user->about }}</div> {{--todo auto paragraphs, share on github--}}
   </section>
-@elseif ($is_me)
+@elseif ($user->isMe())
   @include('user/gag/about-user')
 @endif
