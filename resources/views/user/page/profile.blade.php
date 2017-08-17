2c06775bedde6be577ff4d502a76d8809e6312f6
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 02:14:27 +0600

    add profile.blade.php

---
+++ resources/views/user/page/profile.blade.php
@@ -0,0 +1,45 @@
+@php
+  /**
+   * @var \App\Model\User $user
+   * @var boolean $showWelcome
+   * @var boolean $emptyProfile
+   * @var \Illuminate\Database\Eloquent\Collection|\App\Model\Skill[] $userSkills
+   * @var \Illuminate\Database\Eloquent\Collection|\App\Model\Lang[] $userLangs
+   */
+@endphp
+
+@extends('user.layout')
+
+@section('user-content')
+  <div class="user-profile" data-show-welcome="{{ $showWelcome }}">
+    @include('user.page.profile.intro')
+
+    @if (!$emptyProfile || $user->isMe())
+
+      <div class="panel">
+        <div class="panel-body">
+
+          @include('user.page.profile.about')
+          @include('user.page.profile.experience')
+          @include('user.page.profile.education')
+
+          {{--Навыки и языки--}}
+          @if (count($userSkills) || count($userLangs))
+
+            {{--todo add views--}}
+            @include('user.page.profile.skills')
+            @include('user.page.profile.langs')
+
+          @elseif ($user->isMe())
+            @include('user.gag.skills')
+          @endif
+
+          @include('user.page.profile.exams')
+          @include('user.page.profile.subscriptions')
+
+        </div>
+      </div>
+
+    @endif
+  </div>
+@endsection
