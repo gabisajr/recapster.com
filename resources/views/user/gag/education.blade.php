e58ecb1c184d2c59fb007c2b0a9b39cabe707de5
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:43:42 +0600

    add education.blade.php

---
+++ resources/views/user/gag/education.blade.php
@@ -0,0 +1,7 @@
+<div class="gag">
+  <img src="{{ asset('/images/diploma.png') }}" class="gag-img">
+  <div class="gag-text">
+    <strong>{{ __('Не указано образование') }}</strong><br>
+    {{ __('Укажите учебные заведения, которые вы закончили, а также вашу учебную специализацию и достижения.') }}</div>
+  <a href="/edit/education" class="btn btn-primary">{{ __('Добавить место учебы') }}</a>
+</div>
