f501fad3aa57c2b4e9fd0a47e293c6dddd92675d
Lukas Pierce <kosha.industry@gmail.com>
2017-08-18 01:44:03 +0600

    add education.blade.php

---
+++ resources/views/user/page/profile/education.blade.php
@@ -0,0 +1,24 @@
+@php
+  /**
+   * @var \App\Model\User $user
+   * @var \App\Model\UserEducation[] $educations
+   */
+
+  $educations_count = count($educations);
+@endphp
+
+@if ($educations_count)
+  <section class="user-profile-block" id="education">
+    @if ($user->isMe())
+      {{--todo route--}}
+      <a href="/edit/education" class="pencil hidden-print" title="{{ __('Редактировать') }}"></a>
+    @endif
+    <h3 class="user-profile-block-title">{{ __('Образование') }}</h3>
+    @foreach ($educations as $education)
+      {{--todo use blade--}}
+      echo new Post_Education($education);
+    @endforeach
+  </section>
+@elseif ($user->isMe())
+  @include('user.gag.education')
+@endif
