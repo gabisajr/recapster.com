202b0418671248eac1b40c38eca1ea81eb1ffcc1
Lukas Pierce <kosha.industry@gmail.com>
2017-08-21 18:44:46 +0600

    add media-viewer.blade.php

---
+++ resources/views/modal/media-viewer.blade.php
@@ -0,0 +1,21 @@
+<div class="media-viewer media-viewer-animate">
+  <div class="media-panel-header">
+    <div class="user"></div>
+    <div class="menu menu-horizontal media-panel-tools">
+      <div class="menu-item">
+        <button class="icon icon-x-viewer" title="{{ __('Закрыть') }}"></button>
+      </div>
+    </div>
+  </div>
+  <div class="media-content">
+    <button class="btn btn-round btn-media-prev"><span class="icon icon-chevron-left icon-l" title="{{ __('Назад') }}"></span></button>
+    <div class="media">
+      <div class="object-fit object-fit-scaledown object-fit-zoomin">
+        <div class="wrapper">
+          <img class="media-viewer-img">
+        </div>
+      </div>
+    </div>
+    <button class="btn btn-round btn-media-next"><span class="icon icon-chevron-right icon-l" title="{{ __("Далее") }}"></span></button>
+  </div>
+</div>
