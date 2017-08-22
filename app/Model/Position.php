72f7d9cb8b661f638b69bae33f51316d3f0f4cbd
Lukas Pierce <kosha.industry@gmail.com>
2017-08-22 22:28:08 +0600

    update Position model, add hasMany relations: jobs, interviews, reviews, salaries regenerate phpDoc for Position model

--- app/Model/Position.php
+++ app/Model/Position.php
@@ -5,52 +5,56 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Model_Position - Должность
+ * App\Model\Position
  *
- * @property int    id
- * @property string title            - название должности
- * @property string alias            - альяс для ссылки
- * @property int    salaries_count   - кол-во одобренных зарплат
- * @property int    reviews_count    - кол-во одобренных отзывов
- * @property int    interviews_count - кол-во одобренных собеседований
- * @property int    jobs_count       - кол-во активных вакансий
- *
- * ------------------- virtual -----------------------------
- * @property string $of_position     - родительный падеж название должности
- *
- * ------------------- has many ----------------------------
- * @property ORM    salaries         - зарплаты
- * @property ORM    reviews          - отзывы
- * @property ORM    interviews       - собеседования
- * @property ORM    jobs             - вакансии
+ * @property int $id
+ * @property string $title
+ * @property string $alias альяс для ссылки
+ * @property int $salaries_count количество одобренных зарплат
+ * @property int $reviews_count количество одобренных отзывов
+ * @property int $interviews_count количество одобренных собеседований
+ * @property int $jobs_count количество активных вакансий
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Interview[] $interviews
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Job[] $jobs
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Review[] $reviews
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Salary[] $salaries
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position search($search)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereAlias($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereInterviewsCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereJobsCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereReviewsCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereSalariesCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Position whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Position extends Model {
 
-  //todo relation
-  protected $_has_many = [
-    'salaries'   => [
-      'model'       => 'Salary',
-      'foreign_key' => 'position_id',
-    ],
-    'reviews'    => [
-      'model'       => 'Review',
-      'foreign_key' => 'position_id',
-    ],
-    'interviews' => [
-      'model'       => 'Interview',
-      'foreign_key' => 'position_id',
-    ],
-    'jobs'       => [
-      'model'       => 'Job',
-      'foreign_key' => 'position_id',
-    ],
-  ];
+  public function jobs() {
+    return $this->hasMany('App\Model\Job');
+  }
+
+  public function interviews() {
+    return $this->hasMany('App\Model\Interview');
+  }
+
+  public function reviews() {
+    return $this->hasMany('App\Model\Review');
+  }
+
+  public function salaries() {
+    return $this->hasMany('App\Model\Salary');
+  }
 
   /**
    * Scope a query to only include positions whose title or alias likes as $search
    *
    * @param \Illuminate\Database\Eloquent\Builder $query
-   * @param  string                               $search
+   * @param  string $search
    * @return \Illuminate\Database\Eloquent\Builder
    */
   public function scopeSearch($query, $search) {
@@ -73,54 +77,6 @@ class Position extends Model {
         ['mb_strtolower'], //todo filter
       ],
     ];
-  }
-
-  public function recount_all() {
-    $this->reviews_count = $this->reviews->where('status', '=', Status::APPROVED)->count_all();
-    $this->salaries_count = $this->salaries->where('status', '=', Status::APPROVED)->count_all();
-    $this->interviews_count = $this->interviews->where('status', '=', Status::APPROVED)->count_all();
-    $this->jobs_count = $this->jobs->where('status', '=', Status::APPROVED)->count_all();
-    return $this->save();
-  }
-
-  /**
-   * пересчитать и сохранить кол-во одобренных отзывов
-   *
-   * @return Model_Company
-   */
-  public function recount_reviews() {
-    $this->reviews_count = $this->reviews->where('status', '=', Status::APPROVED)->count_all();
-    return $this->save();
-  }
-
-  /**
-   * пересчитать и сохранить кол-во одобренных зарплат
-   *
-   * @return Model_Company
-   */
-  public function recount_salaries() {
-    $this->salaries_count = $this->salaries->where('status', '=', Status::APPROVED)->count_all();
-    return $this->save();
-  }
-
-  /**
-   * пересчитать и сохранить кол-во одобренных собеседований
-   *
-   * @return Model_Company
-   */
-  public function recount_interviews() {
-    $this->interviews_count = $this->interviews->where('status', '=', Status::APPROVED)->count_all();
-    return $this->save();
-  }
-
-  /**
-   * пересчитать и сохранить кол-во одобренных вакансий
-   *
-   * @return Model_Company
-   */
-  public function recount_jobs() {
-    $this->jobs_count = $this->jobs->where('status', '=', Status::APPROVED)->count_all();
-    return $this->save();
   }
 
   public static function route_filter_alias($route, $params, $request) {
