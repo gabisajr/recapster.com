9edfca750ab85f33e29c21ed2a526388b30e2408
Lukas Pierce <kosha.industry@gmail.com>
2017-08-23 04:58:26 +0600

    use routes instead raw url

--- resources/views/user/edit/menu.blade.php
+++ resources/views/user/edit/menu.blade.php
@@ -7,8 +7,8 @@
   <div class="panel-body pad-vert-sm">
     <menu role="menu" class="aside-menu">
       {{--todo use routes--}}
-      <a class="aside-menu-item{{ $active == 'personal' ? ' active' : '' }}" href="/edit/personal">{{ __('Личная информация') }}</a>
-      <a class="aside-menu-item{{ $active == 'contacts' ? ' active' : '' }}" href="/edit/contacts">{{ __('Контакты') }}</a>
+      <a class="aside-menu-item{{ $active == 'personal' ? ' active' : '' }}" href="{{ route('user.edit.personal') }}">{{ __('Личная информация') }}</a>
+      <a class="aside-menu-item{{ $active == 'contacts' ? ' active' : '' }}" href="{{ route('user.edit.contacts') }}">{{ __('Контакты') }}</a>
       <a class="aside-menu-item{{ $active == 'education' ? ' active' : '' }}" href="/edit/education">{{ __('Образование') }}</a>
       <a class="aside-menu-item{{ $active == 'experience' ? ' active' : '' }}" href="/edit/experience">{{ __('Опыт работы') }}</a>
       <a class="aside-menu-item{{ $active == 'skills' ? ' active' : '' }}" href="/edit/skills">{{ __('Навыки и языки') }}</a>
