b554ee8e6a9c634fe05947e404a94eca9fad622f
Lukas Pierce <kosha.industry@gmail.com>
2017-09-07 03:42:12 +0600

    update server message view

--- resources/views/admin/partials/message.blade.php
+++ resources/views/admin/partials/message.blade.php
@@ -1,35 +1,17 @@
-@if($message_warning = session()->pull('message_warning'))
-  <div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true" title="{{ __("Закрыть") }}">&times;</span>
-    </button>
-    {!! $message_warning !!}
-  </div>
+{{--todo close button--}}
+
+@if($success = Session::pull('message_success'))
+  <div class="alert alert-success">{{ $success }}</div>
 @endif
 
-@if($message_success = session()->pull('message_success'))
-  <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true" title="{{ __("Закрыть") }}">&times;</span>
-    </button>
-    {!! $message_success !!}
-  </div>
+@if($error = Session::pull('message_error'))
+  <div class="alert alert-danger">{{ $error }}</div>
 @endif
 
-@if($message_info = session()->pull('message_info'))
-  <div class="alert alert-info alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true" title="{{ __("Закрыть") }}">&times;</span>
-    </button>
-    {!! $message_info !!}
-  </div>
+@if($warning = Session::pull('message_warning'))
+  <div class="alert alert-warning">{{ $warning }}</div>
 @endif
 
-@if($message_error = session()->pull('message_error'))
-  <div class="alert alert-info alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true" title="{{ __("Закрыть") }}">&times;</span>
-    </button>
-    {!! $message_error !!}
-  </div>
+@if($info = Session::pull('message_info'))
+  <div class="alert alert-info">{{ $info }}</div>
 @endif
