f7f046e8d89429918eda30e1a66f0968a8e162d8
Lukas Pierce <kosha.industry@gmail.com>
2017-09-07 14:09:22 +0600

    rename alias to slug

--- app/Regex.php
+++ app/Regex.php
@@ -4,8 +4,8 @@ namespace App;
 
 class Regex {
 
-  const INPUT_PATTERN_ALIAS = '^[A-Za-z0-9]+[\-A-Za-z0-9]*[A-Za-z0-9]+$';
-  const ALIAS = '/^[A-Za-z0-9]+[\-A-Za-z0-9]*[A-Za-z0-9]+$/';
+  const INPUT_PATTERN_SLUG = '^[A-Za-z0-9]+[\-A-Za-z0-9]*[A-Za-z0-9]+$';
+  const SLUG = '/^[A-Za-z0-9]+[\-A-Za-z0-9]*[A-Za-z0-9]+$/';
   const TWITTER_LOGIN = '/^[A-Za-z0-9_]{1,15}$/';
   const INSTAGRAM_LOGIN = '/^[a-zA-Z0-9._]+$/';
   const USERNAME = '/^[0-9a-z][0-9a-z._-]{1,28}[0-9a-z]$/';
