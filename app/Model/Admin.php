25013cbf7ef88cae8b4a7810cdd35ea440ae51d5
Lukas Pierce <kosha.industry@gmail.com>
2017-09-07 12:22:52 +0600

    add phpDoc for Admin model

--- app/Model/Admin.php
+++ app/Model/Admin.php
@@ -6,6 +6,28 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\PasswordResetNotification;
 
+/**
+ * App\Model\Admin
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property int $is_super_admin
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereIsSuperAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Admin whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Admin extends Authenticatable {
 
   use Notifiable;
