cab5e707b23f8c86044e1fd0f63fa8e7f0e8f693
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 14:14:28 +0600

    add scope ofFaculty to Chair model

--- app/Model/Chair.php
+++ app/Model/Chair.php
@@ -5,14 +5,23 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Model_Faculty - кафедра
- * @property int           $id
- * @property string        $title
- * @property int           $vk_id
+ * App\Model\Chair
  *
- * ------------------ belongs to ---------------
- * @property Model_Faculty $faculty
- * @property int           $faculty_id
+ * @property int $id
+ * @property string $title
+ * @property int|null $faculty_id
+ * @property int|null $vk_id id кафедры ВКонтакте
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Model\Faculty|null $faculty
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair ofFaculty($faculty)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereFacultyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Chair whereVkId($value)
+ * @mixin \Eloquent
  */
 class Chair extends Model {
 
@@ -20,15 +29,32 @@ class Chair extends Model {
     return $this->belongsTo('App\Model\Faculty');
   }
 
-  public function rules() { //todo validation
-    return [
-      'title' => [
-        ['not_empty'],
-      ],
-      'vk_id' => [
-        [[$this, 'unique'], ['vk_id', ':value']],
-      ],
-    ];
+  public function scopeOfFaculty($query, $faculty) {
+    $facultyId = null;
+    if ($faculty instanceof Faculty) {
+      $facultyId = $faculty->id;
+    } elseif (is_numeric($faculty)) {
+      $facultyId = $faculty;
+    } else {
+      error_log("invalid faculty argument: value '$faculty', needs Faculty or int");
+    }
+
+    if ($facultyId) {
+      $query->where('chairs.faculty_id', '=', $facultyId);
+    }
+
+    return $query;
   }
+
+  //public function rules() { //todo validation
+  //  return [
+  //    'title' => [
+  //      ['not_empty'],
+  //    ],
+  //    'vk_id' => [
+  //      [[$this, 'unique'], ['vk_id', ':value']],
+  //    ],
+  //  ];
+  //}
 
 }
