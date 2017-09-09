c0dab1e30296b1b2d1145491e39df366a5a22898
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 14:35:04 +0600

    add item to ru lang config

--- resources/lang/ru/admin.php
+++ resources/lang/ru/admin.php
@@ -3,5 +3,6 @@
 return [
   'import_universities_from_vk' => 'Импорт вузов :city из ВКонтакте',
   'add_faculty_for_university' => 'Добавить факультет для :university',
+  'add_chair_for_faculty' => 'Добавить кафедру для :faculty',
   'chairs_of_faculty' => 'Кафедры (направления) факультета :faculty',
 ];
