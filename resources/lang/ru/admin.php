5fb7fd74b27cf054dbbeba527903a53be38d1358
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 14:07:08 +0600

    add admin localization config for 'ru'

---
+++ resources/lang/ru/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'import_universities_from_vk' => 'Импорт вузов :city из ВКонтакте',
+  'add_faculty_for_university' => 'Добавить факультет для :university',
+  'chairs_of_faculty' => 'Кафедры (направления) факультета :faculty',
+];
