234c1db3f04dd84e882011d177a32e75e49554a1
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 20:26:15 +0600

    use routes

--- resources/views/modal/signup.blade.php
+++ resources/views/modal/signup.blade.php
@@ -3,11 +3,11 @@
     <div class="row signup-modal-row">
       <div class="col-sm-6 col">
         <img src="{{ asset('images/employee.jpg') }}" class="signup-modal-img">
-        <a href="/signup" class="btn btn-primary">{{ __("I'm an applicant") }}</a>
+        <a href="{{ route('signup') }}" class="btn btn-primary">{{ __("I'm an applicant") }}</a>
       </div>
       <div class="col-sm-6 col">
         <img src="{{ asset('images/employer.jpg') }}" class="signup-modal-img">
-        <a href="/signup?employer=1" class="btn btn-primary">{{ __("I'm an employer") }}</a>
+        <a href="{{ route('signup', ['employer' => 1]) }}" class="btn btn-primary">{{ __("I'm an employer") }}</a>
       </div>
     </div>
   </div>
