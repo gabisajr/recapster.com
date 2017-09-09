cef338845f5a00f06862975a448e4648ce820346
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 16:42:16 +0600

    delete 'app-scripts' gulp task

--- gulp/app.js
+++ gulp/app.js
@@ -24,27 +24,12 @@ gulp.task('app-styles', function () {
     ;
 });
 
-gulp.task('app-scripts', function () {
-  return gulp.src('resources/assets/js/**/*.js')
-    .pipe(uglify())
-    .on('error', swallowError)
-    .pipe(gulp.dest('public/js'))
-    .pipe(livereload())
-    ;
-});
-
-function swallowError(error) {
-  console.error(error.toString());
-  this.emit('end');
-}
-
 module.exports = function () {
   gulp.watch('resources/assets/sass/**/*.scss', ['app-styles']);
-  gulp.watch('resources/assets/js/**/*.js', ['app-scripts']);
   gulp.watch('resources/views/**/*.blade.php', function () {
     livereload.reload();
   });
 
   // force run
-  gulp.run(['app-styles', 'app-scripts']);
+  gulp.run(['app-styles']);
 };
