df327502b2019a643eb73dbcfe68f0a9bb51a26c
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 16:13:50 +0600

    update phpDoc for EducationForm model

--- app/Model/EducationForm.php
+++ app/Model/EducationForm.php
@@ -5,13 +5,21 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class EducationForm - форма обучения
- * @property int    $id
- * @property string $title - название
- * @property string $vk_education_form
- * @property int    $sort
- * @property string $created_at
- * @property string $updated_at
+ * App\Model\EducationForm
+ *
+ * @property int $id
+ * @property string $title название формы обучения
+ * @property string $vk_education_form форма обучения ВКонтакте
+ * @property int $sort
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereSort($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\EducationForm whereVkEducationForm($value)
+ * @mixin \Eloquent
  */
 class EducationForm extends Model {
 
