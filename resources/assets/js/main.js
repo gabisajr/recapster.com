f599b543262c2852badeeda3ed859892ef52dd21
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 22:22:42 +0600

    ES6 syntax

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,59 +1,61 @@
-define(['jquery', 'String'], function ($) {
+import $ from 'jquery';
+import 'bootstrap';
 
-  requirejs(['Popper'], function (Popper) {
-    window.Popper = Popper;
-    requirejs(['bootstrap']);
-  });
+import './header';
 
-  requirejs(['header']);
-
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': window.app.csrfToken
-    }
-  });
-
-  //<editor-fold desc="tooltips">
-  $(document).ready(function () {
-    //todo use bootstrap 4 tooltips http://v4-alpha.getbootstrap.com/components/tooltips/
-    // requirejs(['powertip'], function (powertip) {
-    //   powertip($('.powertip'));
-    // });
-  });
-  //</editor-fold>
-
-  //<editor-fold desc="textarea maxlength && autosize">
-  (function () {
-    $('textarea[maxlength]')
-      .wrap('<div class="maxlength-wrap"></div>')
-      .before('<span class="minor gray maxlength-counter"></span>')
-      .bind('updateCounter', updateCounter)
-      .each(updateCounter)
-      .keypress(updateCounter)
-      .keyup(updateCounter);
-
-    function updateCounter() {
-      var $textarea = $(this)
-        , $counter = $textarea.siblings('.maxlength-counter')
-        , max = $textarea.attr('maxlength')
-        , len = $textarea.val().length;
-
-      $counter.text(len + ' из ' + max + ' символов');
-    }
-
-    requirejs(['autosize'], function (autosize) {
-      autosize($('textarea.autosize'));
-    });
-  })();
-  //</editor-fold>
-
-  //<editor-fold desc="модалка с котиками регистрации">
-  (function () {
-    if ($('.open-signup-modal').length) requirejs(['modal/signup']);
-    if ($('.open-signin-modal').length) requirejs(['modal/signin']);
-  })();
-  //</editor-fold>
-
-  requirejs(['modal/alert']);
-
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': window.app.csrfToken
+  }
 });
+
+//bootstrap tooltips
+//todo replace all .powertip to attr data-toggle="tooltip"
+$('[data-toggle="tooltip"]').tooltip();
+
+
+{ //textarea autosize
+
+  let $ta = $('textarea.autosize');
+  if ($ta.length) require.ensure(['autosize'], function (require) {
+    let autosize = require('autosize');
+    autosize($ta);
+  });
+}
+
+
+{ //textarea maxlength
+  $('textarea[maxlength]')
+    .wrap('<div class="maxlength-wrap"></div>')
+    .before('<span class="minor gray maxlength-counter"></span>')
+    .bind('updateCounter', updateCounter)
+    .each(updateCounter)
+    .keypress(updateCounter)
+    .keyup(updateCounter);
+
+  function updateCounter() {
+    let $textarea = $(this)
+      , $counter = $textarea.siblings('.maxlength-counter')
+      , max = $textarea.attr('maxlength')
+      , len = $textarea.val().length;
+
+    $counter.text(len + ' из ' + max + ' символов');
+  }
+}
+
+{ //модалка с котиками регистрации
+
+  if ($('.open-signup-modal').length) {
+    require.ensure(['modal-signup'], function (require) {
+      require('modal-signup');
+    });
+  }
+
+  if ($('.open-signin-modal').length) {
+    require.ensure(['modal-signin'], function (require) {
+      require('modal-signin');
+    });
+  }
+}
+
+//requirejs(['modal/alert']);
