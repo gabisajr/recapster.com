6e3b6f8ff5dd1691b9b4681befa3d789fabf46cb
Lukas Pierce <kosha.industry@gmail.com>
2017-09-09 16:04:34 +0600

    update phpDoc for UserEducation model

--- app/Model/UserEducation.php
+++ app/Model/UserEducation.php
@@ -5,33 +5,45 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Model_Education
- * @property int                    $id
- * @property int                    $start_year  - год начала обучения
- * @property int                    $start_month - месяц начала обучения
- * @property string                 $specialty   - специальность
- * @property int                    $end_year    - год окончания обучения
- * @property int                    $end_month   - месяц окончания обучения
- * @property int                    $text        - описание (специализация и достижения)
+ * App\Model\UserEducation
  *
- * ---------------- belongs to: --------------
- * @property Model_University       $university
- * @property int                    $university_id
- *
- * @property Model_Faculty          $faculty
- * @property int                    $faculty_id
- *
- * @property Model_Chair            $chair
- * @property int                    $chair_id
- *
- * @property Model_Education_Form   $edu_form
- * @property int                    $edu_form_id
- *
- * @property Model_User             $user
- * @property int                    $user_id
- *
- * @property Model_Education_Status $status
- * @property int                    $status_id
+ * @property int $id
+ * @property int $user_id пользователь
+ * @property int|null $university_id университет
+ * @property int|null $faculty_id факультет
+ * @property int|null $chair_id кафедра/направление
+ * @property int|null $edu_form_id
+ * @property int|null $start_year год начала обучения
+ * @property int|null $start_month месяц начала обучения
+ * @property int|null $end_year год окончания обучения
+ * @property int|null $end_month месяц окончания обучения
+ * @property string $text описание (специализация и достижения)
+ * @property string|null $specialty специальность
+ * @property int|null $status_id статус образования
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Model\Chair|null $chair
+ * @property-read \App\Model\EducationForm|null $educationForm
+ * @property-read \App\Model\EducationStatus|null $educationStatus
+ * @property-read \App\Model\Faculty|null $faculty
+ * @property-read \App\Model\University|null $university
+ * @property-read \App\Model\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereChairId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereEduFormId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereEndMonth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereEndYear($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereFacultyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereSpecialty($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereStartMonth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereStartYear($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereUniversityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereUserId($value)
+ * @mixin \Eloquent
  */
 class UserEducation extends Model {
 
