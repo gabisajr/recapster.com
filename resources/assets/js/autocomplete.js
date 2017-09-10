0678dbea984b89e2b34f0f6bbdf3c746b713b848
Lukas Pierce <kosha.industry@gmail.com>
2017-09-10 19:08:33 +0600

    ES6 syntax autocompletePosition works via GraphQL api

--- resources/assets/js/autocomplete.js
+++ resources/assets/js/autocomplete.js
@@ -1,106 +1,102 @@
-define(['jquery', 'jquery-ui', 'highlight'], function ($) {
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/autocomplete';
+import 'jquery-highlight';
 
-  $.fn.extend({
-    autocompletePosition: function () {
-      this.autocomplete({
-        minLength: 1,
-        source: function (request, response) {
-          $.ajax({
-            dataType: 'json',
-            url: '/position/search',
-            data: {q: request.term},
-            success: function (positions) {
-              positions = positions.map(function (position) {
-                position.label = position.title;
-                delete position.title;
-                return position;
-              });
-              return response(positions);
-            }
+$.fn.extend({
+  autocompletePosition: function () {
+    this.autocomplete({
+      minLength: 1,
+      source: function (request, response) {
+        let positions = graphql('/graphql')(`query { positions (search: "${request.term}") {id, title} }`);
+        positions().then(data => {
+          let positions = data.positions.map(function (position) {
+            position.label = position.title;
+            delete position.title;
+            return position;
           });
-        }
-      });
+          return response(positions);
+        });
+      }
+    });
 
-      this.autocomplete("instance")._renderItem = function (ul, position) {
-        var $li = $('<li><div>' + position.label + '</div></li>');
-        $li.highlight(this.term);
-        return $li.appendTo(ul);
-      };
+    this.autocomplete("instance")._renderItem = function (ul, position) {
+      let $li = $('<li><div>' + position.label + '</div></li>');
+      $li.highlight(this.term);
+      return $li.appendTo(ul);
+    };
 
-      return this;
-    },
+    return this;
+  },
 
-    autocompleteCompany: function (params) {
-      params = params || {};
-      this.autocomplete({
-        minLength: 2,
-        source: function (request, response) {
-          $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: '/autocomplete/company',
-            data: {filter: request.term, use_link: params.use_link, admin_link: params.admin_link},
-            success: response
-          });
-        }
-      });
+  autocompleteCompany: function (params) {
+    params = params || {};
+    this.autocomplete({
+      minLength: 2,
+      source: function (request, response) {
+        $.ajax({
+          type: 'POST',
+          dataType: 'json',
+          url: '/autocomplete/company',
+          data: {filter: request.term, use_link: params.use_link, admin_link: params.admin_link},
+          success: response
+        });
+      }
+    });
 
-      this.autocomplete("instance")._renderItem = function (ul, item) {
-        var li = $(item.html);
-        li.find('.title').highlight(this.term);
-        return li.appendTo(ul);
-      };
+    this.autocomplete("instance")._renderItem = function (ul, item) {
+      let li = $(item.html);
+      li.find('.title').highlight(this.term);
+      return li.appendTo(ul);
+    };
 
-      return this;
-    },
+    return this;
+  },
 
-    autocompleteCity: function () {
-      this.autocomplete({
-        minLength: 2,
-        source: function (request, response) {
-          $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: '/autocomplete/city',
-            data: {filter: request.term},
-            success: response
-          });
-        }
-      });
+  autocompleteCity: function () {
+    this.autocomplete({
+      minLength: 2,
+      source: function (request, response) {
+        $.ajax({
+          type: 'POST',
+          dataType: 'json',
+          url: '/autocomplete/city',
+          data: {filter: request.term},
+          success: response
+        });
+      }
+    });
 
-      this.autocomplete("instance")._renderItem = function (ul, item) {
-        var li = $(item.html);
-        li.find('.city-title').highlight(this.term);
-        return li.appendTo(ul);
-      };
+    this.autocomplete("instance")._renderItem = function (ul, item) {
+      let li = $(item.html);
+      li.find('.city-title').highlight(this.term);
+      return li.appendTo(ul);
+    };
 
-      return this;
-    },
+    return this;
+  },
 
-    autocompleteUser: function (params) {
-      params = params || {};
-      this.autocomplete({
-        minLength: 2,
-        source: function (request, response) {
-          $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: '/autocomplete/user',
-            data: {filter: request.term, use_link: params.use_link, admin_link: params.admin_link},
-            success: response
-          });
-        }
-      });
+  autocompleteUser: function (params) {
+    params = params || {};
+    this.autocomplete({
+      minLength: 2,
+      source: function (request, response) {
+        $.ajax({
+          type: 'POST',
+          dataType: 'json',
+          url: '/autocomplete/user',
+          data: {filter: request.term, use_link: params.use_link, admin_link: params.admin_link},
+          success: response
+        });
+      }
+    });
 
-      this.autocomplete("instance")._renderItem = function (ul, item) {
-        var li = $(item.html);
-        li.find('.title').highlight(this.term);
-        return li.appendTo(ul);
-      };
+    this.autocomplete("instance")._renderItem = function (ul, item) {
+      let li = $(item.html);
+      li.find('.title').highlight(this.term);
+      return li.appendTo(ul);
+    };
 
-      return this;
-    }
-
-  });
+    return this;
+  }
 
 });
