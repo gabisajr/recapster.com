be553fa0b800322cf37bdf52f5d346567907fe50
Lukas Pierce <kosha.industry@gmail.com>
2017-09-10 16:58:41 +0600

    add Faculties GraphQL query

---
+++ app/GraphQL/Query/FacultiesQuery.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\Model\Faculty;
+
+class FacultiesQuery extends Query {
+
+  protected $attributes = [
+    'name' => 'faculties',
+  ];
+
+  public function type() {
+    return Type::listOf(GraphQL::type('Faculty'));
+  }
+
+  public function args() {
+    return [
+      'id'         => ['name' => 'id', 'type' => Type::int()],
+      'university' => ['name' => 'university', 'type' => Type::int()],
+    ];
+  }
+
+  public function resolve($root, $args) {
+
+    $query = Faculty::query();
+
+    //фильтр по id
+    if ($id = array_get($args, 'id')) {
+      $query->where('id', '=', $id);
+    }
+
+    //фильтр по университету
+    if ($university = array_get($args, 'university')) {
+      $query->ofUniversity($university);
+    }
+
+    return $query->get();
+  }
+
+}
