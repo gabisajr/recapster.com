29ed41f6d8532b0dbd6b4c3726ee59349dcbb385
Lukas Pierce <kosha.industry@gmail.com>
2017-09-10 20:27:33 +0600

    ES6 syntax

--- resources/assets/js/edit/personal.js
+++ resources/assets/js/edit/personal.js
@@ -1,73 +1,75 @@
-define(['jquery', 'i18n', 'browser', 'fileupload', 'autocomplete', 'select2'], function ($, __) {
+import '../main';
+import $ from 'jquery';
+import 'fileupload';
+import 'select2';
+import '../autocomplete';
 
-  var $form = $('form#edit-personal-form')
-    , $submit = $form.find('.btn-submit')
-    , $position = $form.find(':input#position')
-    , $country = $form.find(':input#country')
-    , $city = $form.find(':input#city')
-    , $status = $form.find(':input#status');
+let $form = $('form#edit-personal-form')
+  , $submit = $form.find('.btn-submit')
+  , $position = $form.find(':input#position')
+  , $country = $form.find(':input#country')
+  , $city = $form.find(':input#city')
+  , $status = $form.find(':input#status');
 
-  //загрузка аватарки
-  (function () {
-    var $avatar = $form.find('img.avatar')
-      , $wrapper = $avatar.closest('.wrapper')
-      , $loader = $('<div class="panel-loader"><img src="/images/loading.svg" class="loader"></div>').appendTo($wrapper).hide();
+//загрузка аватарки
+(function () {
+  let $avatar = $form.find('img.avatar')
+    , $wrapper = $avatar.closest('.wrapper')
+    , $loader = $('<div class="panel-loader"><img src="/images/loading.svg" class="loader"></div>').appendTo($wrapper).hide();
 
-    $form.find('input#avatar').fileupload({
-      url: '/user/avatar/upload',
-      beforeSend: function () {
-        $loader.fadeIn();
-      },
-      complete: function () {
-        $loader.fadeOut();
-      },
-      done: function (e, data) {
-        $avatar.attr('src', data.result);
-      }
-    });
-
-  })();
-
-  //смена статуса
-  $status.change(function () {
-    var status = $(this).val()
-      , $jobSettingsLink = $form.find('a.job-search-settings');
-    if (status === 'search' || status === 'ready') {
-      $jobSettingsLink.show();
-    } else {
-      $jobSettingsLink.hide();
+  $form.find('input#avatar').fileupload({
+    url: '/user/avatar/upload',
+    beforeSend: function () {
+      $loader.fadeIn();
+    },
+    complete: function () {
+      $loader.fadeOut();
+    },
+    done: function (e, data) {
+      $avatar.attr('src', data.result);
     }
   });
 
-  $country.select2({
-    placeholder: __("Выберите страну")
-  }).on('select2:opening', function () {
-    $(this).data('select2').$dropdown.find(':input.select2-search__field').attr('placeholder', __('Начните набирать') + '…');
-  });
+})();
 
-  $city.select2({
-    placeholder: __("Выберите город")
-  }).on('select2:opening', function () {
-    $(this).data('select2').$dropdown.find(':input.select2-search__field').attr('placeholder', __('Начните набирать') + '…');
-  });
-
-  //при смене страны подгружаем список городов
-  $country.change(function () {
-    var countryId = $(this).val();
-    $.ajax({
-      url: '/country/cities',
-      data: {country: countryId},
-      type: 'post',
-      success: function (cities) {
-        var options = '<option value="">' + __('Город') + '</option>';
-        $.each(cities, function (index, city) {
-          options += '\n<option value="' + city.id + '">' + city.title + '</option>';
-        });
-        $city.html(options);
-      }
-    })
-  });
-
-  $position.autocompletePosition();
-
+//смена статуса
+$status.change(function () {
+  let status = $(this).val()
+    , $jobSettingsLink = $form.find('a.job-search-settings');
+  if (status === 'search' || status === 'ready') {
+    $jobSettingsLink.show();
+  } else {
+    $jobSettingsLink.hide();
+  }
 });
+
+$country.select2({
+  placeholder: __("Choose your country")
+}).on('select2:opening', function () {
+  $(this).data('select2').$dropdown.find(':input.select2-search__field').attr('placeholder', __('Start typing') + '…');
+});
+
+$city.select2({
+  placeholder: __("Choose your city")
+}).on('select2:opening', function () {
+  $(this).data('select2').$dropdown.find(':input.select2-search__field').attr('placeholder', __('Start typing') + '…');
+});
+
+//при смене страны подгружаем список городов
+$country.change(function () {
+  let countryId = $(this).val();
+  $.ajax({
+    url: '/country/cities',
+    data: {country: countryId},
+    type: 'post',
+    success: function (cities) {
+      let options = '<option value="">' + __('City') + '</option>';
+      $.each(cities, function (index, city) {
+        options += '\n<option value="' + city.id + '">' + city.title + '</option>';
+      });
+      $city.html(options);
+    }
+  })
+});
+
+$position.autocompletePosition();
