11f99f4614bfd14e4c7ea263f8feae6de7cc75d5
Lukas Pierce <kosha.industry@gmail.com>
2017-09-11 00:20:32 +0600

    add GraphQL Countries query

---
+++ app/GraphQL/Query/CountriesQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\Model\Country;
+
+class CountriesQuery extends Query {
+
+  protected $attributes = [
+    'name' => 'countries',
+  ];
+
+  public function type() {
+    return Type::listOf(GraphQL::type('Country'));
+  }
+
+  public function args() {
+    return [
+      'id' => ['name' => 'id', 'type' => Type::int()],
+    ];
+  }
+
+  public function resolve($root, $args) {
+
+    $query = Country::query();
+
+    //фильтр по id
+    if ($id = array_get($args, 'id')) {
+      $query->where('id', '=', $id);
+    }
+
+    return $query->get();
+  }
+
+}
