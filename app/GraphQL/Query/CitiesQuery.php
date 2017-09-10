10da14d250ab53683ccd9f8b3d866d6817cab31e
Lukas Pierce <kosha.industry@gmail.com>
2017-09-10 15:33:24 +0600

    add GraphQL CitiesQuery

---
+++ app/GraphQL/Query/CitiesQuery.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\Model\City;
+
+class CitiesQuery extends Query {
+
+  protected $attributes = [
+    'name' => 'cities',
+  ];
+
+  public function type() {
+    return Type::listOf(GraphQL::type('City'));
+  }
+
+  public function args() {
+    return [
+      'id'              => ['name' => 'id', 'type' => Type::int()],
+      'slug'            => ['name' => 'slug', 'type' => Type::string()],
+      'title'           => ['name' => 'title', 'type' => Type::string()],
+      'country'         => ['name' => 'country', 'type' => Type::int()],
+      'hasUniversities' => ['name' => 'hasUniversities', 'type' => Type::boolean()],
+    ];
+  }
+
+  public function resolve($root, $args) {
+
+    $query = City::query();
+
+    //фильтр по id
+    if ($id = array_get($args, 'id')) {
+      $query->where('id', '=', $id);
+    }
+
+    //фильтр по slug
+    if ($slug = array_get($args, 'slug')) {
+      $query->where('slug', '=', $slug);
+    }
+
+    //фильтр по стране
+    if ($country = array_get($args, 'country')) {
+      $query->ofCountry($country);
+    }
+
+    //города с университетами
+    if ($hasUniversities = array_get($args, 'hasUniversities')) {
+      $query->has('universities');
+    }
+
+    return $query->get();
+  }
+
+}
