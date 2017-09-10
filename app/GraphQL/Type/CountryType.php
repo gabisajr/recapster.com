2bfda8cbdaa899ff12c5b30f16e741b7ff2eae77
Lukas Pierce <kosha.industry@gmail.com>
2017-09-11 00:29:18 +0600

    add GraphQL CountryType

---
+++ app/GraphQL/Type/CountryType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use App\Model\Country;
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as GraphQLType;
+
+class CountryType extends GraphQLType {
+
+  protected $attributes = [
+    'name'        => 'Country',
+    'description' => 'A country',
+  ];
+
+  public function fields() {
+    return [
+      'id'           => [
+        'type'        => Type::nonNull(Type::int()),
+        'description' => 'The id of the city',
+      ],
+      'title'        => [
+        'type'        => Type::string(),
+        'description' => 'Title of the city in the real world',
+      ],
+      'iso_code'     => [
+        'type'        => Type::string(),
+        'description' => 'The unique country code by standart ISO 3166-1 alpha-2',
+      ],
+      'cities'       => [
+        'type'        => Type::listOf(GraphQL::type('City')),
+        'description' => 'Cities in this country',
+        'args'        => [
+          'id' => ['name' => 'id', 'type' => Type::int()],
+        ],
+        'resolve'     => function (Country $country, $args) {
+          $query = $country->cities();
+          if ($id = array_get($args, 'id')) {
+            $query->where('id', '=', $id);
+          }
+          return $query->get();
+        },
+      ],
+      'universities' => [
+        'type'        => Type::listOf(GraphQL::type('University')),
+        'description' => 'Universities in this country',
+      ],
+    ];
+  }
+
+}
