08ddf49965583fb12e9c1b2f364965a6e1a19ae1
Lukas Pierce <kosha.industry@gmail.com>
2017-09-10 17:15:15 +0600

    add GraphQL chairs query

---
+++ app/GraphQL/Query/ChairsQuery.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\Model\Faculty;
+
+class ChairsQuery extends Query {
+
+  protected $attributes = [
+    'name' => 'chairs',
+  ];
+
+  public function type() {
+    return Type::listOf(GraphQL::type('Chair'));
+  }
+
+  public function args() {
+    return [
+      'id'      => ['name' => 'id', 'type' => Type::int()],
+      'faculty' => ['name' => 'faculty', 'type' => Type::int()],
+    ];
+  }
+
+  public function resolve($root, $args) {
+
+    $query = Faculty::query();
+
+    //фильтр по id
+    if ($id = array_get($args, 'id')) {
+      $query->where('id', '=', $id);
+    }
+
+    //фильтр по факультету
+    if ($faculty = array_get($args, 'faculty')) {
+      $query->ofFaculty($faculty);
+    }
+
+    //default order
+    $query->orderBy('title');
+
+    return $query->get();
+  }
+
+}
