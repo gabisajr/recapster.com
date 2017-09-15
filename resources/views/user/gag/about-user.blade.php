b6063e30ad04503c7d2b4c544e01e7b34b711e5c
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 02:00:37 +0600

    update

--- resources/views/user/gag/about-user.blade.php
+++ resources/views/user/gag/about-user.blade.php
@@ -2,7 +2,7 @@
   <img src="{{ asset('/images/notebook.png') }}" class="gag-img">
   <div class="gag-text">
     <strong>{{ __('Нет информации о себе') }}</strong><br>
-    {{ __('Расскажите о себе так, чтобы заинтересовать интересных вам работодателей.') }}
+    {{ __('Расскажите о себе так, чтобы привлечь интересных вам работодателей.') }}
   </div>
   {{--todo route--}}
   <a href="/edit/personal" class="btn btn-primary">{{ __('Рассказать о себе') }}</a>
