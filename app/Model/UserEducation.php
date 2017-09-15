25e68f5bcc0fcf61f156301619d429ae54b44a5d
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 01:13:08 +0600

    rename relations: educationForm -> form educationStatus -> status

--- app/Model/UserEducation.php
+++ app/Model/UserEducation.php
@@ -17,15 +17,15 @@ use Illuminate\Database\Eloquent\Model;
  * @property int|null $start_month месяц начала обучения
  * @property int|null $end_year год окончания обучения
  * @property int|null $end_month месяц окончания обучения
- * @property string $text описание (специализация и достижения)
+ * @property string|null $text описание (специализация и достижения)
  * @property string|null $specialty специальность
  * @property int|null $status_id статус образования
  * @property \Carbon\Carbon|null $created_at
  * @property \Carbon\Carbon|null $updated_at
  * @property-read \App\Model\Chair|null $chair
- * @property-read \App\Model\EducationForm|null $educationForm
- * @property-read \App\Model\EducationStatus|null $educationStatus
  * @property-read \App\Model\Faculty|null $faculty
+ * @property-read \App\Model\EducationForm|null $form
+ * @property-read \App\Model\EducationStatus|null $status
  * @property-read \App\Model\University|null $university
  * @property-read \App\Model\User $user
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\UserEducation whereChairId($value)
@@ -63,11 +63,11 @@ class UserEducation extends Model {
     return $this->belongsTo('App\Model\Chair');
   }
 
-  public function educationForm() {
+  public function form() {
     return $this->belongsTo('App\Model\EducationForm', 'edu_form_id');
   }
 
-  public function educationStatus(){
+  public function status(){
     return $this->belongsTo('App\Model\EducationStatus', 'status_id');
   }
 
