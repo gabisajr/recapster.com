c9d2e876f7155014bdeb7c2943bb6d25fd13c7d3
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 16:02:13 +0600

    rename CityType -> City

--- app/GraphQL/Type/City.php
+++ app/GraphQL/Type/City.php
@@ -6,7 +6,7 @@ use GraphQL;
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Type as GraphQLType;
 
-class CityType extends GraphQLType {
+class City extends GraphQLType {
 
   protected $attributes = [
     'name'        => 'City',
