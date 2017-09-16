0ddcef6d5ef80735d1b40c8f5f7f7592c01b23c6
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 22:27:18 +0600

    add Periodable trait

---
+++ app/Model/Periodable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model;
+
+use App;
+
+/**
+ * Trait Periodable
+ * @package App\Model
+ * @property int|null $start_month начало, месяц
+ * @property int|null $start_year начало, год
+ * @property int|null $end_month окончание работы, месяц
+ * @property int|null $end_year окончание работы, год
+ */
+trait Periodable {
+
+}
