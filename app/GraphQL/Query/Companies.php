5dc7fee142b730a0b205ddf67b62a56a24e11811
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 16:41:04 +0600

    add arguments: search, confirmed to companies GraphQL query

--- app/GraphQL/Query/Companies.php
+++ app/GraphQL/Query/Companies.php
@@ -19,17 +19,30 @@ class Companies extends Query {
 
   public function args() {
     return [
-      'id' => ['name' => 'id', 'type' => Type::int()],
+      'id'        => ['name' => 'id', 'type' => Type::int(), 'description' => "Get a company by id"],
+      'search'    => ['type' => Type::string(), 'description' => "Find companies by title or slug"],
+      'confirmed' => ['type' => Type::boolean(), 'description' => "Set <i>true</i> for return only confirmed companies"],
     ];
   }
 
   public function resolve($root, $args) {
 
+    /** @var Company $query */
     $query = Company::query();
 
     //фильтр по id
     if ($id = array_get($args, 'id')) {
       $query->where('id', '=', $id);
+    }
+
+    //фильтр по строке поиска
+    if ($search = array_get($args, 'search')) {
+      $query->search($search);
+    }
+
+    //только подтвержденые компании
+    if ($confirmed = array_get($args, 'confirmed')) {
+      $query->confirmed();
     }
 
     return $query->get();
