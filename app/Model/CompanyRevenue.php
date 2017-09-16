0c3d8189f8225e7ba4f925433558e68e315d27cd
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 16:08:19 +0600

    update CompanyRevenue model: add companies relation generate phpDoc

--- app/Model/CompanyRevenue.php
+++ app/Model/CompanyRevenue.php
@@ -5,20 +5,25 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Model_Company доход компании
+ * App\Model\CompanyRevenue
  *
- * @property int             id
- * @property string          title
- * @property Model_Company[] companies
+ * @property int $id
+ * @property string $title
+ * @property int $sort
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\Company[] $companies
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\CompanyRevenue whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\CompanyRevenue whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\CompanyRevenue whereSort($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\CompanyRevenue whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\CompanyRevenue whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class CompanyRevenue extends Model {
 
-  //todo relation
-  protected $_has_many = [
-    'companies' => [
-      'model'       => 'Company',
-      'foreign_key' => 'revenue_id',
-    ],
-  ];
+  public function companies() {
+    return $this->hasMany('App\Model\Company', 'revenue_id');
+  }
 
 }
