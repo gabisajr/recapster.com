4ad22d95caa5c93f208414f90e6494319dfb99f3
Lukas Pierce <kosha.industry@gmail.com>
2017-09-16 21:51:15 +0600

    user route

--- resources/views/user/edit/menu.blade.php
+++ resources/views/user/edit/menu.blade.php
@@ -10,7 +10,7 @@
       <a class="aside-menu-item{{ $active == 'personal' ? ' active' : '' }}" href="{{ route('user.edit.personal') }}">{{ __('Личная информация') }}</a>
       <a class="aside-menu-item{{ $active == 'contacts' ? ' active' : '' }}" href="{{ route('user.edit.contacts') }}">{{ __('Контакты') }}</a>
       <a class="aside-menu-item{{ $active == 'education' ? ' active' : '' }}" href="{{ route('user.edit.education') }}">{{ __('Образование') }}</a>
-      <a class="aside-menu-item{{ $active == 'experience' ? ' active' : '' }}" href="/edit/experience">{{ __('Опыт работы') }}</a>
+      <a class="aside-menu-item{{ $active == 'experience' ? ' active' : '' }}" href="{{ route('user.edit.experience') }}">{{ __('Опыт работы') }}</a>
       <a class="aside-menu-item{{ $active == 'skills' ? ' active' : '' }}" href="/edit/skills">{{ __('Навыки и языки') }}</a>
       <a class="aside-menu-item{{ $active == 'exams' ? ' active' : '' }}" href="/edit/exams">{{ __('Тесты, экзамены и курсы') }}</a>
     </menu>
